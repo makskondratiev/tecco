ac5da5fa9239dafaa7f577159b78b7d0dc91eed7
Ilya <rewasd91@mail.ru>
2017-02-10 14:30:46 +0300

    адаптив services

--- services.php
+++ services.php
@@ -44,7 +44,7 @@
 				</ul>
 			</div>
 			<div class="squareblock services__quote">
-				<p class="services__quotetext"><span class="services__quotes">«</span><br>
+				<p class="services__quotetext">
 				Безумно остроумная цитата, которая проливает свет на крутость компании и прошаренность ее руководителя</p>
 				<p class="services__quoteauthor">Максим Кондратьев<br>Tecco IT CEO</p>
 			</div>
@@ -56,10 +56,12 @@
 				<h2 class="content__heading">IT outsourcing</h2>
 				<p class="content__smalltext">Иногда ввиду многих факторов бизнес сталкивается с необходимостью формирования  отдаленного штата сотрудников, включающего команду разработчиков с тим-лидом, тестировщиков, руководителя проекта и др. специалистов со всей необходимой инфраструктурой.<br><br>
 				Tecco располагает всей необходимой компетенцией и возможностями для создания отдаленных команд и организации их труда согласно поставленным задачам.</p>
-				<div class="squareblock squareblock_dark services__moreinfo">
+				<a href="#" class="squareblock squareblock_dark services__moreinfo">
 					<p class="content__text">Для получения более подробной информации об услугах компании</p>
-					<p class="content__text"><a class="content__text_link" href="#">Свяжитесь с нами<img src="img/icon_arrow_3.svg" alt="@@" width="26" height="31"></a></p>
-				</div>
+					<p class="content__text content__text_link" href="#">Свяжитесь с нами
+						<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.16 30.39" width="26" height="31" fill="#221f1f"><title>icon_arrow_3</title><path d="M-47.26,887.51a1.52,1.52,0,0,0-.55.11h0a1.49,1.49,0,0,0-.49.33l-11,11a1.5,1.5,0,0,0,0,2.12,1.5,1.5,0,0,0,2.12,0l8.47-8.47v23.77a1.5,1.5,0,0,0,1.5,1.5,1.5,1.5,0,0,0,1.5-1.5V892.63l8.57,8.57a1.5,1.5,0,0,0,2.12,0,1.49,1.49,0,0,0,.44-1.06,1.5,1.5,0,0,0-.44-1.06L-46.2,888a1.49,1.49,0,0,0-.49-0.33h0a1.52,1.52,0,0,0-.55-0.11" transform="translate(59.8 -887.51)"/></svg>
+					</p>
+				</a>
 			</div>
 		</div>
 	</div>
